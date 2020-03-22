89750c7090aa9cc0593e809ce13c9f531a92dc1f
Kris Temmerman <kris@krisdevs.com>
2020-03-22 18:52:46 +0100

    added SEO component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
 import React from "react"
+import SEO from "../components/shared/seo"
+import { graphql } from "gatsby"
 
 
-const IndexPage = () => (
-  <h1>Hello there</h1>
-)
+const IndexPage = (props) => {
+  const { data } = props
+  return (
+    <>
+      <SEO title="Home" description={data.site.siteMetadata.description} />
+
+      <h1>Hello there</h1>
+    </>
+  )
+}
 
 export default IndexPage
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
